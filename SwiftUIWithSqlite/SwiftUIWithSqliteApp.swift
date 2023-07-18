85fb7b3982538d2dc4b0313a1450dc59d084705e
mistj <tyradccc@gmail.com>
2023-07-18 17:40:55 +0800

    Initial Commit

---
+++ SwiftUIWithSqlite/SwiftUIWithSqliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithSqliteApp.swift
+//  SwiftUIWithSqlite
+//
+//  Created by mist on 2023/7/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithSqliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
